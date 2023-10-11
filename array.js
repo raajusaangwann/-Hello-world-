07b821b0ba102e7f36d99618478cc2db43da6e30
raajusaangwann <145984417+raajusaangwann@users.noreply.github.com>
2023-10-11 17:33:40 +0530

    added rahul to the objected

---
+++ array.js
@@ -0,0 +1,100 @@
+// let numbers = new Array(5);
+// numbers.unshift("hello", "google");
+// numbers.splice(2,2,"what is happening in world");
+// console.log(numbers);
+// console.log(numbers.length);
+ 
+//console.log(red);
+
+// let arr1=[{name : " jonny"}, {name : "michael"}];
+// let arr2 =arr1;
+// arr2.push({name : "adam smith"});
+// console.log(arr1);
+// console.log(arr2);
+// console.log("+++++++++++++");
+// arr1[0].name=" david hussey";
+// console.log(arr1);
+// console.log("+++++++++++++-----------------");
+
+// let arr = ['h', 'e'];
+// arr.name = 'John';
+
+// console.log(arr); // ["h", "e"]
+// console.log(arr.name); // "John"
+// console.log(arr['name']); // "John"
+
+// const fruits = ['apple', 'banana', 'orange', 'kiwi', 'pear'];
+// fruits.push("grape");
+// console.log(fruits);
+// fruits.pop();
+// fruits.shift();
+// console.log(fruits);
+
+// const myArray = [];
+// console.log(myArray.length);
+// myArray.push(["apple", "orange"]);
+// myArray.push(["grape", "pear"]);
+// myArray.push(["pineapple", "mango"]);
+
+// console.log(myArray);   
+
+/* Output
+[["apple", "orange"], ["grape", "pear"], ["pineapple", "mango"]]
+*/
+
+
+let x = [
+    ['Jack', 24],
+    ['Sara', 25],
+    ['Peter', 26]
+    ];
+    
+    // accessing the first item
+    //console.log(x[1]); // ["Jack", 24]
+    
+    // accessing the first item of the first inner array
+    //console.log(x[0][1]); // Jack
+    
+    // accessing the second item of the third inner array
+    //console.log(x[2][0]); // 26
+    // let stu_data=[["raam", 24],["laxman", 23]];
+    // stu_data.push(['bharat',22]);
+    // stu_data[2][2]="hello";
+    // stu_data[1].push("namaste");
+    //  //console.log(stu_data.splice(0,0,'hello'));
+    //  console.log(stu_data);
+    //  console.log('############################');
+    // for (let i of stu_data){
+    //     for(let j of i){
+    //         console.log(j);
+    //     }
+    // }
+
+//     const name = `johnn`;
+//     console.log(typeof name);
+        
+//     let a = 'hello';
+// //a = 'H';
+// console.log("HELLOOO WORLD".split(''));
+// console.log(a.length); // "hello"
+//  const ab= 2345;
+//  const c = true ;
+ 
+// const res1= String(ab);
+// const res2= String(c);
+// console.log(res1);
+// console.log(typeof res1);
+// console.log(res2);
+// const namee = 'My name is \\peterrr.';
+// console.log(namee);   // My name is 'Peter'.
+
+var myArray = [1, 2, 3, 4, 5];
+var newArray = myArray.slice(2, 3);
+console.log(newArray);
+ 
+let obj ={
+    name : " rahul",
+    hobby : " travelling",
+    mob_num : 98911123413,
+};
+console.log(obj);
